db085881bfedb505d0f2234a5eac6797a52bb28b
Patryk <patrykokpara@hotmail.com>
2022-09-20 23:57:22 +0100

    First bug

--- deb.rb
+++ deb.rb
@@ -1,5 +1,5 @@
 def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     ciphertext_chars = plaintext.chars.map do |char|
       (65 + cipher.find_index(char)).chr
     end
